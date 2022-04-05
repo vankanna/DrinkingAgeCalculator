ce1b82c964b9de712d9342710667b6e394453c95
vankanna <passvankanna@gmail.com>
2022-04-05 17:44:04 -0500

    done

---
+++ AwesomeApp/AwesomeApp/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AwesomeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            myDatePicker.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+        }
+
+        private void myButton_Clicked(object sender, EventArgs e)
+        {
+
+            var currentYear = DateTime.Now.Year;
+            var yearOfBirth = myDatePicker.Date.Year;
+            var country = (String)picker.SelectedItem;
+            Message.Text = country;
+            var age = currentYear - yearOfBirth;
+            var ageLimit = 21;
+            if (country == "USA")
+            {
+                ageLimit = 21;
+            }
+            else if (country == "South Korea")
+            {
+                ageLimit = 19;
+            }
+            else if (country == "Japan")
+            {
+                ageLimit = 20;
+            }
+            else if (country == "Germany")
+            {
+                ageLimit = 16;
+            }
+
+            
+            if (age >= ageLimit)
+            {
+                Message.Text = $"You can legally drink!";
+            }
+            else
+            {
+                Message.Text = $"You have {ageLimit - age} more years until you can drink in {country}!";
+            }
+
+
+        }
+
+    }
+}
